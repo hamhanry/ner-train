d41703790da7847635a32dee5ef9fdf972c9cba8
Hanry Ham <hamhanry@gmail.com>
2023-10-16 21:19:34 +0700

    add : general metric

---
+++ ner_train/utils/metric.py
@@ -0,0 +1,45 @@
+import math
+from typing import Dict, List, Optional
+
+import numpy as np
+import torch
+
+class AverageMeter(object):
+    """Computes and stores the average and current value"""
+
+    def __init__(self, name, fmt=":f"):
+        self.name = name
+        self.fmt = fmt
+        self.val = 0.0
+        self.avg = 0.0
+        self.sum = 0.0
+        self.count = 0.0
+        self.reset()
+
+    def reset(self):
+        self.val = 0.0
+        self.avg = 0.0
+        self.sum = 0.0
+        self.count = 0.0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+    def __str__(self):
+        fmtstr = "{name} {val" + self.fmt + "} ({avg" + self.fmt + "})"
+        return fmtstr.format(**self.__dict__)
+
+@torch.no_grad()
+def topk_accuracy(output, target, topk=(1,)) -> List[torch.Tensor]:
+    """Computes the accuracy over the k top predictions for the specified values of k"""
+    maxk = max(topk)
+    batch_size = target.size(0)
+
+    _, pred = output.topk(maxk, 1, True, True)
+    pred = pred.t()
+    correct = pred.eq(target.view(1, -1).expand_as(pred))
+
+    return [correct[:k].reshape(-1).float().sum(0) * 100.0 / batch_size for k in topk]
